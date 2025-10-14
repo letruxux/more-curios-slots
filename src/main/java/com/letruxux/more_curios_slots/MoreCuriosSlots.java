377d1619053527abd4db93877dad66ccc8d2730c
riccardo <102257756+letruxux@users.noreply.github.com>
2025-10-14 16:03:19 +0000

    idk

--- src/main/java/com/letruxux/more_curios_slots/MoreCuriosSlots.java
+++ src/main/java/com/letruxux/more_curios_slots/MoreCuriosSlots.java
@@ -5,62 +5,61 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 @Mod(MoreCuriosSlots.MODID)
 public class MoreCuriosSlots {
+  public static final String MODID = "more_curios_slots";
 
-    public static final String MODID = "more_curios_slots";
+  public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(NeoForgeRegistries.ITEMS, MODID);
 
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+  public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister
+      .create(Registries.CREATIVE_MODE_TAB, MODID);
 
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+  public static final List<String> ALL_CURIO_NAMES = List.of(
+      "curio",
+      "head",
+      "feet",
+      "hands",
+      "charm",
+      "belt",
+      "ring",
+      "back",
+      "bracelet",
+      "necklace",
+      "body"
+  // "calendar",
+  );
 
-    public static final List<String> ALL_CURIO_NAMES = List.of(
-            "curio",
-            "head",
-            "feet",
-            "hands",
-            "charm",
-            "belt",
-            "ring",
-            "back",
-            "bracelet",
-            "necklace",
-            "body"
-            // "calendar",
-    );
+  public static final List<DeferredRegister.Item<ExtraSlotItem>> EXTRA_SLOT_ITEMS = ALL_CURIO_NAMES.stream()
+      .map(slot -> ITEMS.registerItem(
+          String.format("extra_%s_slot", slot),
+          (properties) -> new ExtraSlotItem(properties, slot),
+          new Item.Properties()))
+      .toList();
 
-    public static final List<RegistryObject<ExtraSlotItem>> EXTRA_SLOT_ITEMS =
-            ALL_CURIO_NAMES.stream()
-                    .map(slot -> ITEMS.register(
-                            String.format("extra_%s_slot", slot),
-                            () -> new ExtraSlotItem(new Item.Properties(), slot)
-                    ))
-                    .toList();
+  public static final Supplier<CreativeModeTab> MORE_CURIOS_SLOTS_TAB = CREATIVE_MODE_TABS
+      .register("more_curios_slots_tab", () -> CreativeModeTab.builder()
+          .withTabsBefore(CreativeModeTabs.COMBAT)
+          .title(Component.literal("More Curios Slots"))
+          .icon(() -> EXTRA_SLOT_ITEMS.get(0).get().getDefaultInstance())
+          .displayItems((parameters, output) -> {
+            for (DeferredRegister.Item<ExtraSlotItem> extraSlotItem : EXTRA_SLOT_ITEMS) {
+              output.accept(extraSlotItem.get());
+            }
+          }).build());
 
+  public MoreCuriosSlots(FMLJavaModLoadingContext context) {
+    IEventBus modEventBus = context.getModEventBus();
 
-    public static final RegistryObject<CreativeModeTab> MORE_CURIOS_SLOTS_TAB = CREATIVE_MODE_TABS.register("more_curios_slots_tab", () -> CreativeModeTab.builder()
-            .withTabsBefore(CreativeModeTabs.COMBAT)
-            .title(Component.literal("More Curios Slots"))
-            .icon(() -> EXTRA_SLOT_ITEMS.get(0).get().getDefaultInstance())
-            .displayItems((parameters, output) -> {
-                for (RegistryObject<ExtraSlotItem> extraSlotItem : EXTRA_SLOT_ITEMS) {
-                    output.accept(extraSlotItem.get());
-                }
-            }).build());
-
-    public MoreCuriosSlots(FMLJavaModLoadingContext context) {
-        IEventBus modEventBus = context.getModEventBus();
-
-        ITEMS.register(modEventBus);
-        CREATIVE_MODE_TABS.register(modEventBus);
-    }
+    ITEMS.register(modEventBus);
+    CREATIVE_MODE_TABS.register(modEventBus);
+  }
 }
