296d2cb87e12ef17275619a5ee6405c580cbe511
letruxux <mrgianfranco483@gmail.com>
2025-10-14 16:21:04 +0200

    Remove unused

--- src/main/java/com/letruxux/more_curios_slots/ExtraSlotItem.java
+++ src/main/java/com/letruxux/more_curios_slots/ExtraSlotItem.java
@@ -1,14 +1,8 @@
 package com.letruxux.more_curios_slots;
 
 import net.minecraft.ChatFormatting;
-import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.tags.ItemTags;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -19,31 +13,16 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.alchemy.Potion;
-import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import top.theillusivec4.curios.api.CuriosApi;
-import top.theillusivec4.curios.api.type.ISlotType;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 public class ExtraSlotItem extends Item {
     private final String SlotNameString;
-
-    private boolean isSlotInCurios() {
-        TagKey<Item> tag = ItemTags.create(new ResourceLocation("curios", SlotNameString)); // replace "planks" with your tag
-        List<Item> itemsInTag = ForgeRegistries.ITEMS.getValues()
-                .stream()
-                .filter(item -> item.builtInRegistryHolder().is(tag))
-                .toList();
-        return !itemsInTag.isEmpty();
-    }
 
     public ExtraSlotItem(Properties properties, String slot) {
         super(properties.rarity(Rarity.RARE));
@@ -61,16 +40,6 @@ public class ExtraSlotItem extends Item {
                         .withStyle(ChatFormatting.GRAY)
         );
 
-        /*if (level != null && level.isClientSide()) {
-            boolean slotExists = isSlotInCurios();
-            if (!slotExists) {
-                components.add(
-                        Component.literal("This slot is not available in your modpack! It will be useless.")
-                                .withStyle(ChatFormatting.RED)
-                );
-            }
-        }*/
-
         super.appendHoverText(itemStack, level, components, tooltipFlag);
     }
 
@@ -78,16 +47,6 @@ public class ExtraSlotItem extends Item {
     @Override
     public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level level, @NotNull Player player, @NotNull InteractionHand hand) {
         ItemStack itemStack = player.getItemInHand(hand);
-
-        /*if (!isSlotInCurios()) {
-            if (!level.isClientSide) {
-                player.displayClientMessage(Component.literal(String.format("There is no mod in this modpack that supports the %s slot. You can't unlock this.", this.SlotNameString)).withStyle(ChatFormatting.RED), true);
-            } else {
-                player.playSound(SoundEvents.VILLAGER_HURT);
-            }
-
-            return super.use(level, player, hand);
-        }*/
 
         if (!level.isClientSide) {
             player.displayClientMessage(Component.literal(String.format("You just unlocked +1 %s slot!", this.SlotNameString)), true);
